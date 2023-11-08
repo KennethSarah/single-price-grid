da6346b6e7be9c241a0537938c1fbf6de5720080
Kenneth Sarah <kennethnifemi@gmail.com>
2023-11-08 23:07:00 +0100

    grid

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('btn-id');
+
+function addParagraph() {
+    /* const newElement = document.createElement('p');
+    const pContent = 'You already have an account, go to login page!!';
+    newElement.textContent = pContent; */
+    console.log('The button was clicked');
+
+    window.alert('You already have an account, go to login page!')
+}
